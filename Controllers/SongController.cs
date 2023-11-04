18fe433642baaf85c272fb7378995a0cc6bedd9f
daniel-mar <danielmar.alex@gmail.com>
2023-11-04 16:52:27 -0400

    OneSong view added, buttons liking and disliking, logic to handle seconds <10 seconds to include 0 before integer. Delete button if loggedin user owns viewed song. SongController routing/querying for creating, viewing OneSong

--- Controllers/SongController.cs
+++ Controllers/SongController.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS8629
 using musicShare.Models;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
@@ -18,17 +19,50 @@ public class SongController : Controller
 
     // Song Routing
 
-    
+
     [HttpGet("song/create")]
     public IActionResult AddSong()
     {
-        if(HttpContext.Session.GetInt32("UserId") == null)
+        if (HttpContext.Session.GetInt32("UserId") == null)
         {
             return RedirectToAction("Index");
         }
         ViewBag.NotLoggedIn = false;
         return View();
     }
+
+    // Handle new song creation
+    [HttpPost("song/add")]
+    public IActionResult CreateSong(Song newSong)
+    {
+        ViewBag.NotLoggedIn = false;
+        if (ModelState.IsValid)
+        {
+            newSong.UserId = (int)HttpContext.Session.GetInt32("UserId");
+            _context.Add(newSong);
+            _context.SaveChanges();
+            return Redirect($"/song/{newSong.SongId}");
+        }
+        else
+        {
+            return View("AddSong");
+        }
+    }
+
+    // After creation, redirect to view one song
+    [HttpGet("song/{songId}")]
+    public IActionResult OneSong(int songId)
+    {
+        ViewBag.NotLoggedIn = false;
+        Song? songToShow = _context.Songs.Include(s => s.Submitter).Include(d => d.UsersWhoLiked).FirstOrDefault(a => a.SongId == songId);
+        if (songToShow == null)
+        {
+            return RedirectToAction("AllSongs");
+        }
+        return View(songToShow);
+    }
+
+
 
 
 }
